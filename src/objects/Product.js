6c7220c123de6fb3118d3010f8488214006d970f
guesung <gueit214@naver.com>
2024-11-06 11:07:00 +0900

    feat: `Product`와 `Promotion`객체로 상품과 할인 정보를 분리

---
+++ src/objects/Product.js
@@ -0,0 +1,14 @@
+class Product {
+  #products;
+
+  constructor(products) {
+    this.#products = products;
+  }
+
+  /**
+   * 1. 상품을 등록한다.
+   * 2. 상품을 재고한다.
+   */
+}
+
+export default Product;
---
+++ src/objects/Promotion.js
@@ -0,0 +1,14 @@
+class Promotion {
+  #promotions;
+
+  constructor(promotions) {
+    this.#promotions = promotions;
+  }
+
+  /**
+   * 1. 할인 정보를 등록한다.
+   * 2. 할인 정보를 확인한다.
+   */
+}
+
+export default Promotion;
