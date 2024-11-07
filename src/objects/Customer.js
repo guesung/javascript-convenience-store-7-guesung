c2a7024ff2e957974f394ee0cac084637c2a2b2f
guesung <gueit214@naver.com>
2024-11-07 12:47:16 +0900

    feat: 로또 손님을 `Customer`로 구현

--- src/objects/Customer.js
+++ src/objects/Customer.js
@@ -1,14 +1,63 @@
-import { InputController } from '../controllers/index.js';
+import { InputController, OutputController } from '../controllers/index.js';
 import OrderHistory from './OrderHistory.js';
 
 class Customer {
   #orderHistory;
   #receipt;
 
-  static async order() {
+  constructor() {
+    this.#receipt = [];
+  }
+
+  async order() {
     const items = await InputController.readItems();
 
-    return new OrderHistory(items);
+    this.#orderHistory = new OrderHistory(items);
+  }
+
+  async checkPromotion(product) {
+    for await (const [item, quantity] of this.#orderHistory.orderMap) {
+      const promotionInfo = product.getPromotionInfo(item);
+      const isProductLeft = product.getIsProductLeft(item, quantity + 1);
+
+      if (!isProductLeft) continue;
+
+      if (quantity === promotionInfo.buy) {
+        const answer = await InputController.askOneMoreFree(item);
+        if (answer === 'Y') this.#orderHistory.addQuantity(item);
+      }
+      if (quantity < promotionInfo.buy) {
+        const answer = await InputController.askMoreForPromotion(
+          item,
+          quantity,
+        );
+        if (answer === 'Y') this.#orderHistory.addQuantity(item);
+      }
+    }
+  }
+
+  calculateOrder(product) {
+    for (const [item, quantity] of this.#orderHistory.orderMap) {
+      const promotionInfo = product.getPromotionInfo(item);
+      const price = product.getPrice(item);
+
+      const promotionQuantity = Math.floor(
+        quantity / (promotionInfo.buy + promotionInfo.get),
+      );
+
+      this.#receipt.push({
+        quantity,
+        price,
+        name: item,
+        promotionQuantity,
+      });
+
+      product.reduceProduct(item, quantity);
+    }
+  }
+
+  calculateAll(isMembershipDiscount) {
+    OutputController.printReceipt(this.#receipt, isMembershipDiscount);
   }
 }
 
