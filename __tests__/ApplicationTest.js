ee416a060281dd73cb838c819462bb5a0d11732e
guesung <gueit214@naver.com>
2024-11-07 13:45:12 +0900

    test: 예외 케이스 추가 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,4 +1,5 @@
 import App from '../src/App.js';
+import { ERROR_MESSAGE } from '../src/lib/constants.js';
 import {
   expectLogContains,
   expectLogContainsWithoutSpacesAndEquals,
@@ -45,54 +46,86 @@ describe('편의점', () => {
     jest.restoreAllMocks();
   });
 
-  test('파일에 있는 상품 목록 출력', async () => {
-    await run({
-      inputs: ['[콜라-1]', 'N', 'N'],
-      expected: [
-        '- 콜라 1,000원 10개 탄산2+1',
-        '- 콜라 1,000원 10개',
-        '- 사이다 1,000원 8개 탄산2+1',
-        '- 사이다 1,000원 7개',
-        '- 오렌지주스 1,800원 9개 MD추천상품',
-        '- 오렌지주스 1,800원 재고 없음',
-        '- 탄산수 1,200원 5개 탄산2+1',
-        '- 탄산수 1,200원 재고 없음',
-        '- 물 500원 10개',
-        '- 비타민워터 1,500원 6개',
-        '- 감자칩 1,500원 5개 반짝할인',
-        '- 감자칩 1,500원 5개',
-        '- 초코바 1,200원 5개 MD추천상품',
-        '- 초코바 1,200원 5개',
-        '- 에너지바 2,000원 5개',
-        '- 정식도시락 6,400원 8개',
-        '- 컵라면 1,700원 1개 MD추천상품',
-        '- 컵라면 1,700원 10개',
-      ],
+  describe('정상적인 경우', () => {
+    test('파일에 있는 상품 목록을 출력한다.', async () => {
+      await run({
+        inputs: ['[콜라-1]', 'N', 'N'],
+        expected: [
+          '- 콜라 1,000원 10개 탄산2+1',
+          '- 콜라 1,000원 10개',
+          '- 사이다 1,000원 8개 탄산2+1',
+          '- 사이다 1,000원 7개',
+          '- 오렌지주스 1,800원 9개 MD추천상품',
+          '- 오렌지주스 1,800원 재고 없음',
+          '- 탄산수 1,200원 5개 탄산2+1',
+          '- 탄산수 1,200원 재고 없음',
+          '- 물 500원 10개',
+          '- 비타민워터 1,500원 6개',
+          '- 감자칩 1,500원 5개 반짝할인',
+          '- 감자칩 1,500원 5개',
+          '- 초코바 1,200원 5개 MD추천상품',
+          '- 초코바 1,200원 5개',
+          '- 에너지바 2,000원 5개',
+          '- 정식도시락 6,400원 8개',
+          '- 컵라면 1,700원 1개 MD추천상품',
+          '- 컵라면 1,700원 10개',
+        ],
+      });
+    });
+
+    test('여러 개의 일반 상품 구매한다.', async () => {
+      await run({
+        inputs: ['[비타민워터-3],[물-2],[정식도시락-2]', 'N', 'N'],
+        expectedIgnoringWhiteSpaces: ['내실돈18,300'],
+      });
+    });
+
+    test('기간에 해당하지 않는 프로모션은 적용하지 않는다.', async () => {
+      mockNowDate('2024-02-01');
+
+      await run({
+        inputs: ['[감자칩-2]', 'N', 'N'],
+        expectedIgnoringWhiteSpaces: ['내실돈3,000'],
+      });
     });
   });
 
-  test('여러 개의 일반 상품 구매', async () => {
-    await run({
-      inputs: ['[비타민워터-3],[물-2],[정식도시락-2]', 'N', 'N'],
-      expectedIgnoringWhiteSpaces: ['내실돈18,300'],
+  describe('예외 케이스', () => {
+    test('재고 수량을 초과하여 입력할 경우 예외를 처리한다.', async () => {
+      await runExceptions({
+        inputs: ['[컵라면-12]', 'N', 'N'],
+        inputsToTerminate: INPUTS_TO_TERMINATE,
+        expectedErrorMessage: ERROR_MESSAGE.overQuantity,
+      });
     });
-  });
 
-  test('기간에 해당하지 않는 프로모션 적용', async () => {
-    mockNowDate('2024-02-01');
-
-    await run({
-      inputs: ['[감자칩-2]', 'N', 'N'],
-      expectedIgnoringWhiteSpaces: ['내실돈3,000'],
+    test('형식에 올바르지 않게 작성할 경우 예외를 처리한다.', async () => {
+      await runExceptions({
+        inputs: ['[컵라면-12-3]', 'N', 'N'],
+        inputsToTerminate: INPUTS_TO_TERMINATE,
+        expectedErrorMessage: ERROR_MESSAGE.notFormat,
+      });
+      await runExceptions({
+        inputs: ['[컵라면-12', 'N', 'N'],
+        inputsToTerminate: INPUTS_TO_TERMINATE,
+        expectedErrorMessage: ERROR_MESSAGE.notFormat,
+      });
     });
-  });
 
-  test('예외 테스트', async () => {
-    await runExceptions({
-      inputs: ['[컵라면-12]', 'N', 'N'],
-      inputsToTerminate: INPUTS_TO_TERMINATE,
-      expectedErrorMessage:
-        '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
+    test('없는 상품을 입력할 경우 예외를 처리한다.', async () => {
+      await runExceptions({
+        inputs: ['[컵볶이-3]', 'N', 'N'],
+        inputsToTerminate: INPUTS_TO_TERMINATE,
+        expectedErrorMessage: ERROR_MESSAGE.noItem,
+      });
+    });
+
+    test('0개의 상품을 입력할 경우 예외를 처리한다.', async () => {
+      await runExceptions({
+        inputs: ['[컵볶이-3]', 'N', 'N'],
+        inputsToTerminate: INPUTS_TO_TERMINATE,
+        expectedErrorMessage: ERROR_MESSAGE.zeroQuantity,
+      });
     });
   });
 });
