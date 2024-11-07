8923f56d3ff21c3ab3a5da2c5a7e22f982065dae
guesung <gueit214@naver.com>
2024-11-07 15:33:24 +0900

    refactor: 축약어 제거

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,7 +4,7 @@ export const ERROR_MESSAGE = {
   notFormat: `${ERROR_MESSAGE_DEFAULT} 형식에 맞지 않습니다. [사이다-2],[감자칩-1]와 같은 형식으로 작성해주세요.`,
   noItem: `${ERROR_MESSAGE_DEFAULT} 존재하지 않는 상품입니다.`,
   zeroQuantity: `${ERROR_MESSAGE_DEFAULT} 1개 이상의 상품을 입력해주세요.`,
-  notYOrN: `${ERROR_MESSAGE_DEFAULT} Y 혹은 N을 입력해주세요.`,
+  notYesOrNo: `${ERROR_MESSAGE_DEFAULT} Y 혹은 N을 입력해주세요.`,
 };
 
 export const INPUT_MEESAGE = {
