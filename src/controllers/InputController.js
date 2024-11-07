ed9805a829df7fabde816cdffcbc982625320f59
guesung <gueit214@naver.com>
2024-11-07 13:49:06 +0900

    feat: Y/N응답에 대한 에러 처리

--- src/controllers/InputController.js
+++ src/controllers/InputController.js
@@ -45,27 +45,43 @@ class InputController {
   }
 
   static async askOneMoreFree(item) {
-    const answer = await MissionUtils.Console.readLineAsync(
-      `현재 ${item}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`,
-    );
+    return retryWhileCatchedError(async () => {
+      const answer = await MissionUtils.Console.readLineAsync(
+        `현재 ${item}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`,
+      );
 
-    return answer;
+      this.#validateYesOrNo(answer);
+      return answer;
+    });
   }
 
   static async askMoreForPromotion(item, quantity) {
-    const answer = await MissionUtils.Console.readLineAsync(
-      `현재 ${item} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`,
-    );
+    return retryWhileCatchedError(async () => {
+      const answer = await MissionUtils.Console.readLineAsync(
+        `현재 ${item} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`,
+      );
 
-    return answer;
+      this.#validateYesOrNo(answer);
+
+      return answer;
+    });
   }
 
   static async getIsMembershipDiscount() {
-    const answer = await MissionUtils.Console.readLineAsync(
-      '멤버십 할인을 받으시겠습니까? (Y/N)',
-    );
+    return retryWhileCatchedError(async () => {
+      const answer = await MissionUtils.Console.readLineAsync(
+        '멤버십 할인을 받으시겠습니까? (Y/N)',
+      );
 
-    return answer;
+      this.#validateYesOrNo(answer);
+
+      return answer;
+    });
+  }
+
+  static #validateYesOrNo(answer) {
+    const ANSWERLIST = ['Y', 'N'];
+    if (!ANSWERLIST.includes(answer)) throw new Error(ERROR_MESSAGE.notYOrN);
   }
 }
 
