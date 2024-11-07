8cf6f219e5b9bcf6be15b7323e35c4e7911ec14f
guesung <gueit214@naver.com>
2024-11-07 14:20:02 +0900

    fix: 가게의 상품을 인자로 전달

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import InputController from './controllers/InputController.js';
-import { Customer, Store, Worker } from './objects/index.js';
+import { Customer, Store } from './objects/index.js';
 
 class App {
   #store;
   #customer;
-  #worker;
 
   constructor() {
     this.#store = new Store();
     this.#customer = new Customer();
-    this.#worker = new Worker();
   }
 
   async run() {
     this.#store.prepareProducts();
 
-    await this.#customer.order();
+    await this.#customer.order(this.#store.product);
 
     await this.#customer.checkPromotion(this.#store.product);
 
