8cf8324f5c1c32d6c0166fd1b0c76b01110e2d35
guesung <gueit214@naver.com>
2024-11-07 12:46:02 +0900

    feat: App에서 전체 흐름 제어

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import InputController from './controllers/InputController.js';
 import { Customer, Store, Worker } from './objects/index.js';
 
 class App {
+  #store;
   #customer;
   #worker;
-  #store;
 
   constructor() {
+    this.#store = new Store();
     this.#customer = new Customer();
     this.#worker = new Worker();
-    this.#store = new Store();
   }
 
   async run() {
     this.#store.prepareProducts();
+
+    await this.#customer.order();
+
+    await this.#customer.checkPromotion(this.#store.product);
+
+    this.#customer.calculateOrder(this.#store.product);
+
+    const isMembershipDiscount =
+      await InputController.getIsMembershipDiscount();
+
+    this.#customer.calculateAll(isMembershipDiscount);
   }
 }
 
