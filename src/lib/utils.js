3d249790e44cefb4fa3875533c79901719a6ffe5
guesung <gueit214@naver.com>
2024-11-08 15:39:45 +0900

    fix: 날짜 비교의 잘못된 부분 수정

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,3 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-export const getIsDateBetween = (date, startDate, endDAte) =>
-  date >= startDate && startDate <= endDAte;
+export const getIsDateBetween = (date, startDate, endDate) => date >= startDate && date <= endDate;
