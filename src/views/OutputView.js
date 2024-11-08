bb2e69fe5d7b89651e6f98749ee6931930bb7f15
guesung <gueit214@naver.com>
2024-11-08 15:26:15 +0900

    refactor: `OutputView`메서드를 작게 분리

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,8 +1,9 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { OUTPUT_MESSAGE } from '../lib/constants.js';
 
 class OutputView {
   static printHello() {
-    MissionUtils.Console.print('안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n');
+    MissionUtils.Console.print(OUTPUT_MESSAGE.hello);
   }
 
   static printProducts(products) {
@@ -24,16 +25,16 @@ class OutputView {
 
   static #getQuantityOutput(quantity) {
     if (quantity > 0) return `${quantity}개`;
-    return '재고 없음';
+    return OUTPUT_MESSAGE.noQuantity;
   }
 
   static printReceipt(receipt, isMembershipDiscount) {
-    const totalQuantity = receipt.getTotalQuantity();
-    const totalPrice = receipt.getTotalPrice();
-    const promotionPrice = receipt.getTotalPromotionPrice();
-    const membershipDiscount = receipt.getMembershipDiscount(isMembershipDiscount);
-    const realPrice = totalPrice - promotionPrice - membershipDiscount;
+    this.#printTotalProducts(receipt);
+    this.#printPromotionProducts(receipt);
+    this.#printTotalCalculate(receipt, isMembershipDiscount);
+  }
 
+  static #printTotalProducts(receipt) {
     MissionUtils.Console.print('===========W 편의점===========');
     MissionUtils.Console.print('상품명   수량   금액');
     receipt.receipt.forEach((item) => {
@@ -41,16 +42,40 @@ class OutputView {
         `${item.name}   ${item.quantity}   ${item.price.toLocaleString()}`,
       );
     });
+  }
+
+  static #printPromotionProducts(receipt) {
     MissionUtils.Console.print('===========증	 정===========');
     receipt.receipt.forEach((item) => {
       if (item.promotionQuantity > 0)
         MissionUtils.Console.print(`${item.name}   ${item.promotionQuantity}`);
     });
+  }
+
+  static #printTotalCalculate(receipt, isMembershipDiscount) {
+    const { totalQuantity, totalPrice, promotionPrice, membershipDiscount, realPrice } =
+      this.#calculate(receipt, isMembershipDiscount);
+
     MissionUtils.Console.print('==============================');
     MissionUtils.Console.print(`총구매액 ${totalQuantity} ${totalPrice.toLocaleString()}`);
     MissionUtils.Console.print(`행사할인 -${promotionPrice.toLocaleString()}`);
     MissionUtils.Console.print(`멤버십할인 -${membershipDiscount.toLocaleString()}`);
     MissionUtils.Console.print(`내실돈 ${realPrice.toLocaleString()}`);
+  }
+
+  static #calculate(receipt, isMembershipDiscount) {
+    const totalQuantity = receipt.getTotalQuantity();
+    const totalPrice = receipt.getTotalPrice();
+    const promotionPrice = receipt.getTotalPromotionPrice();
+    const membershipDiscount = receipt.getMembershipDiscount(isMembershipDiscount);
+    const realPrice = totalPrice - promotionPrice - membershipDiscount;
+    return {
+      totalQuantity,
+      totalPrice,
+      promotionPrice,
+      membershipDiscount,
+      realPrice,
+    };
   }
 }
 
