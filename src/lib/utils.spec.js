b90810264195f758609ed57f9ecc13fd4ed48b5b
guesung <gueit214@naver.com>
2024-11-08 14:40:53 +0900

    test: 유틸 함수에 대한 테스트 코드 작성

---
+++ src/lib/utils.spec.js
@@ -0,0 +1,16 @@
+import { getIsDateBetween } from './utils.js';
+
+describe('utils', () => {
+  describe('getIsDateBetween', () => {
+    test('날짜 사이에 있을 경우 true를 반환한다.', () => {
+      expect(
+        getIsDateBetween(new Date('2024-06-01'), new Date('2024-05-01'), new Date('2024-07-01')),
+      ).toBeTruthy();
+    });
+    test('날짜 사이에 있지 않을 경우 false를 반환한다.', () => {
+      expect(
+        getIsDateBetween(new Date('2024-06-01'), new Date('2024-04-01'), new Date('2024-05-01')),
+      ).toBeFalsy();
+    });
+  });
+});
