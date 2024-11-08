5230478f11a26b77331c6d3216c04a37c8579179
guesung <gueit214@naver.com>
2024-11-08 15:39:49 +0900

    chore: 상수 추가

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -10,6 +10,11 @@ export const INPUT_MEESAGE = {
   readItem: '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
 };
 
+export const OUTPUT_MESSAGE = {
+  hello: '안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n',
+  noQuantity: '재고 없음',
+};
+
 export const SEPARATOR = ',';
 export const ITEMS_REGEXR = /^\[[가-힣a-zA-Z]+-\d+\]$/;
 export const LINE_BREAK = '\n';
