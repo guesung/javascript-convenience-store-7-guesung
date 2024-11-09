a07a50ac3ee01b8011478a49718f39aba2b84967
guesung <gueit214@naver.com>
2024-11-09 22:06:54 +0900

    refactor: `retryWhileCatchedError`를 유틸로 분리

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,19 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import { NO, YES } from './constants.js';
 
 export const getIsDateBetween = (date, startDate, endDate) => date >= startDate && date <= endDate;
 export const checkYesOrNo = (answer) => {
   if (answer === YES) return true;
   if (answer === NO) return false;
+};
+export const retryWhileCatchedError = async (callbackFunction, tryCount = 0) => {
+  try {
+    return await callbackFunction();
+  } catch (error) {
+    if (tryCount > 10) return null;
+
+    MissionUtils.Console.print(error.message);
+
+    return await retryWhileCatchedError(callbackFunction, tryCount + 1);
+  }
 };
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,11 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { INPUT_MEESAGE, YES } from '../lib/constants.js';
 import { InputValidator, InputParser } from '../helpers/index.js';
-import { checkYesOrNo } from '../lib/utils.js';
+import { checkYesOrNo, retryWhileCatchedError } from '../lib/utils.js';
 
 class InputView {
   static async readItems(store) {
-    return this.#retryWhileCatchedError(async () => {
+    return retryWhileCatchedError(async () => {
       const rawItems = await MissionUtils.Console.readLineAsync(INPUT_MEESAGE.readItem);
       InputValidator.validateItemsFormat(rawItems);
       const items = InputParser.parseItems(rawItems);
@@ -16,7 +16,7 @@ class InputView {
   }
 
   static async readFreeProduct(item) {
-    return this.#retryWhileCatchedError(async () => {
+    return retryWhileCatchedError(async () => {
       const answer = await MissionUtils.Console.readLineAsync(`현재 ${item}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`);
 
       InputValidator.validateYesOrNo(answer);
@@ -26,7 +26,7 @@ class InputView {
   }
 
   static async readBuyWithoutPromotion(item, quantity) {
-    return this.#retryWhileCatchedError(async () => {
+    return retryWhileCatchedError(async () => {
       const answer = await MissionUtils.Console.readLineAsync(`현재 ${item} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`);
 
       InputValidator.validateYesOrNo(answer);
@@ -36,7 +36,7 @@ class InputView {
   }
 
   static async readtIsMembershipDiscount() {
-    return this.#retryWhileCatchedError(async () => {
+    return retryWhileCatchedError(async () => {
       const answer = await MissionUtils.Console.readLineAsync(INPUT_MEESAGE.membershipDiscount);
 
       InputValidator.validateYesOrNo(answer);
@@ -54,25 +54,13 @@ class InputView {
   }
 
   static async #readIsMoreOrder() {
-    return this.#retryWhileCatchedError(async () => {
+    return retryWhileCatchedError(async () => {
       const answer = await MissionUtils.Console.readLineAsync(INPUT_MEESAGE.tryMoreOrder);
 
       InputValidator.validateYesOrNo(answer);
 
       return checkYesOrNo(answer);
     });
-  }
-
-  static async #retryWhileCatchedError(callbackFunction, tryCount = 0) {
-    try {
-      return await callbackFunction();
-    } catch (error) {
-      if (tryCount > 10) return null;
-
-      MissionUtils.Console.print(error.message);
-
-      return await this.#retryWhileCatchedError(callbackFunction, tryCount + 1);
-    }
   }
 }
 
