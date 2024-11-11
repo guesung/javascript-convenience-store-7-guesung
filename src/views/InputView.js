9e5855170eaf0cd7fc376f506520c4948c873ed2
guesung <gueit214@naver.com>
2024-11-11 20:43:42 +0900

    refactor: InputView에서 OutputView호출하는 로직 제거

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -2,12 +2,10 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import { InputParser, InputValidator } from '../helpers/index.js';
 import { INPUT_MEESAGE } from '../lib/constants.js';
 import { checkYesOrNo, retryWhileCatchedError } from '../lib/utils.js';
-import OutputView from './OutputView.js';
 
 class InputView {
   static async readItems(productModel) {
     return retryWhileCatchedError(async () => {
-      OutputView.printBlank();
       const rawItems = await this.#readLineAsync(INPUT_MEESAGE.readItem);
       InputValidator.validateItemsFormat(rawItems);
       const items = InputParser.parseItems(rawItems);
@@ -20,7 +18,6 @@ class InputView {
 
   static async readIsFreeProduct(item) {
     return retryWhileCatchedError(async () => {
-      OutputView.printBlank();
       const answer = await this.#readLineAsync(`현재 ${item}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`);
 
       InputValidator.validateYesOrNo(answer);
@@ -31,7 +28,6 @@ class InputView {
 
   static async readIsBuyWithoutPromotion(item, quantity) {
     return retryWhileCatchedError(async () => {
-      OutputView.printBlank();
       const answer = await this.#readLineAsync(`현재 ${item} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`);
 
       InputValidator.validateYesOrNo(answer);
@@ -42,7 +38,6 @@ class InputView {
 
   static async readIsMembershipDiscount() {
     return retryWhileCatchedError(async () => {
-      OutputView.printBlank();
       const answer = await this.#readLineAsync(INPUT_MEESAGE.membershipDiscount);
 
       InputValidator.validateYesOrNo(answer);
@@ -51,17 +46,16 @@ class InputView {
     });
   }
 
-  static async retryWhileOrderFinish(callbackFunction) {
-    await callbackFunction();
+  static async retryWhileOrderFinish(orderFunction) {
+    await orderFunction();
 
     const isMoreOrder = await this.#readIsMoreOrder();
 
-    if (isMoreOrder) await this.retryWhileOrderFinish(callbackFunction);
+    if (isMoreOrder) await this.retryWhileOrderFinish(orderFunction);
   }
 
   static async #readIsMoreOrder() {
     return retryWhileCatchedError(async () => {
-      OutputView.printBlank();
       const answer = await this.#readLineAsync(INPUT_MEESAGE.tryMoreOrder);
 
       InputValidator.validateYesOrNo(answer);
